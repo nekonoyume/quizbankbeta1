bb0129a96814b99e9cc0d8acc7a152d262acf5da
nekonoyume <48373603+nekonoyume@users.noreply.github.com>
2019-03-10 03:32:12 +0900

    Update syntax.py

--- syntax.py
+++ syntax.py
@@ -1,35 +1,28 @@
-#!python
-
-print('''
-
-
-<!DOCTYPE html>
-<html >
-  <head>
-    <title>damazm index</title>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-    <h1><a href="index.html"></a>DAMAZM 문제은행</h1>
-
-    <p>문제은행 proto type</p>
-
-
-
-
-    <ul>
-      <li><a href="1.html">문제 입력(구글독스 설문)</a></li>
-      <li><a href="https://docs.google.com/spreadsheets/d/1xwNrwDl-T0dNETUXvGTv8_4DKaXj-MI7UuQ_sz-23hU/edit?usp=sharing">
-        문제 database(구글독스 스프레드시트)</a></li>
-    </ul>
-
-
-
-
-  </body>
+#!/usr/local/bin/python3
+print("Content-Type: text/html")
+print()
+import cgi
+form = cgi.FieldStorage()
+if 'id' in form:
+    pageId = form["id"].value
+else:
+    pageId = 'Welcome'
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    <li><a href="index.py?id=HTML">HTML</a></li>
+    <li><a href="index.py?id=CSS">CSS</a></li>
+    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>{title}</h2>
+  <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+  </p>
+</body>
 </html>
-
-
-''')
+'''.format(title=pageId))
